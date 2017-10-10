3fbf7f230c9f3968e4385dc3a2a4c89dea92ae14
Gutierrez <10148122@id.ohio.gov>
2017-10-10 09:00:17 -0400

    Revert "Upgraded to latest material version, which disrupted"
    This reverts commit 5293a161eca6f0cf6cbff4b14805be17de622cbc.
    
    Reverting because latest version broke down.

--- src/app/modules/reactive-people-picker/reactive-people-picker.module.ts
+++ src/app/modules/reactive-people-picker/reactive-people-picker.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule }        from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTooltipModule} from '@angular/material';
-import {MatAutocompleteModule} from '@angular/material';
-import {MatProgressBarModule} from '@angular/material';
+import {MdTooltipModule} from '@angular/material';
+import {MdAutocompleteModule} from '@angular/material';
+import {MdProgressBarModule} from '@angular/material';
 
 import {TaxPeoplePickerComponent} from './tax-people-picker-component';
 import {SharepointListsWebService} from 'ng-tax-share-point-web-services-module';
 
 @NgModule({
   imports: [
-    CommonModule, ReactiveFormsModule,BrowserAnimationsModule,MatTooltipModule ,
-  MatAutocompleteModule,MatProgressBarModule
+    CommonModule, ReactiveFormsModule,BrowserAnimationsModule,MdTooltipModule ,
+  MdAutocompleteModule,MdProgressBarModule
   ],
   declarations: [TaxPeoplePickerComponent],
   exports: [TaxPeoplePickerComponent],
